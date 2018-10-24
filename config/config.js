8de25522e233fa85c8491f9fc67ffa7b6b156f52
Git sangl003 <sharvari.sangle@gmail.com>
2018-10-23 20:53:32 -0500

    New commits pk

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   development: {
-    use_env_variable: "DEV_URL",
-    dialect: "mysql"
+    use_env_variable: 'DEV_URL',
+    dialect: 'mysql',
   },
   test: {
-    username: "travis",
+    username: 'travis',
     password: null,
-    database: "testdb",
-    host: "localhost",
-    dialect: "mysql"
+    database: 'testdb',
+    host: 'localhost',
+    dialect: 'mysql',
   },
   production: {
-    use_env_variable: "JAWSDB_URL",
-    dialect: "mysql"
-  }
+    use_env_variable: 'JAWSDB_URL',
+    dialect: 'mysql',
+  },
 };
